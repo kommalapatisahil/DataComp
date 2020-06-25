e4ae25824f8cff8ad21773511c39f934066cdaed
kommalapatisahil <30411847+kommalapatisahil@users.noreply.github.com>
2020-06-25 16:19:40 -0700

    Update pupFFT.m

--- pupFFT.m
+++ pupFFT.m
@@ -1,12 +1,13 @@
-B = imread('pup.jpg');
+B = imread('jakob-kac-Ty_e0ANh_eo-unsplash.png');
 
-subplot(1,2,1);
+subplot(1,3,1);
 imshow(B);
-title('Original Image : 100% Fourier Modes')
+title('100% Fourier Modes')
 Bfft = fft2(B);
+ctr = 1; 
 
+for g = [0.01, 0.001]
 Bsort = sort(abs(Bfft(:))); %flattened
-g = 0.05; %5 percent
 id = (1-g)*length(Bsort);
 idf = floor(id);
 thresh = Bsort(idf); %greatest coef value in sorted list
@@ -15,13 +16,14 @@ indx = abs(Bfft)>(thresh);
 Bnew = Bfft.*indx;
 Bim = ifft2(Bnew);
 
-subplot(1,2,2);
+ctr = ctr +1 ;
+subplot(1,3,ctr);
 imshow(uint8(Bim));
-title('Compressed Image : 5% of the Fourier Modes')
+title(string(g*100)+ '% of Fourier Modes')
 
+end
 
-
-
-
-
-
+%Bim = rgb2gray(uint8(Bim));
+%figure();
+%surf(Bim(50:50:end, 50:50:end));
+%title('Pixel Intensities'); 
